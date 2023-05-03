0ffeb8e3abae3f0eaf12c81759a6b5759d3054af
Alan <alan.fernando.leon.cortez@uabc.edu.mx>
2023-05-03 16:18:49 -0700

    Primer commit

---
+++ Practica6-Colas.c
@@ -0,0 +1,13 @@
+/* PROGRAMA DE SIMULACION DE SUPERMERCADO UTILIZANDO COLAS
+Integrentes del equipo:
+    -Leon Cortez Alan Fernando 1274062
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
